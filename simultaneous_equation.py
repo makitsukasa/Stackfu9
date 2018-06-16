6259ed2e3ef948205b95906ebec9946cffd3dc1e
makitsukasa <allium.fistulosum.39@gmail.com>
2018-06-16 22:18:50 +0900

    bugfix : sim_equ can solve backward jumps

--- simultaneous_equation.py
+++ simultaneous_equation.py
@@ -2,16 +2,16 @@
 #
 # (ex)
 # 	eval({0}) =            len({1}) + 10
-# 	eval({1}) = len({0}) + len({1}) + 15
+# 	eval({1}) = len({0}) + len({1}) + 10
 #
 # equation = [
 # 	[False, True , 10],
-# 	[True , True , 15],
+# 	[True , True , 10],
 # ]
 # solve(equation)
 #
-# {0} = '""++"+"+"+',     eval({0}) = 24, len({0}) = 10
-# {1} = '"""++"+"++""++', eval({1}) = 39, len({1}) = 14
+# {0} = '""+"+"+"+"++0+',       eval({0}) = 33,  len({0}) = 14
+# {1} = '"""+"+"+"++""++-0+0+', eval({1}) = -50, len({1}) = 20
 #
 
 from sys import getrecursionlimit, setrecursionlimit
@@ -19,9 +19,9 @@ from random import random, randrange
 from pick_number import pickNumber
 from main import execPickedNumber
 
-# @param equation [[False, True , 10],[True , True , 15],]
+# @param equation [[False, True , 10], [True , True , 15],]
 # @param ans ['"-', '"-'] when called,
-#            ['""++"+"+"+', '"""++"+"++""++'] when returns True
+#            ['""+"+"+"+"++0+', '"""+"+"+"++""++-0+0+'] when returns True
 # @return succeed(True) or failue(False)
 def solve_recur(equation, ans, row):
 	# succeed all rows
@@ -29,31 +29,55 @@ def solve_recur(equation, ans, row):
 	if row >= len(equation):
 		return True
 
+	#if row == 0:
+		#print('start', [execPickedNumber(ans[i]) for i in range(len(ans))], ans)
+
 	new_ans_evaluated = equation[row][-1]
 	for i in range(len(equation[row]) - 1):
 		if(equation[row][i]):
-			new_ans_evaluated += len(ans[i])
+			# len('0=') + len(ans[i]) + len('^')
+			new_ans_evaluated += len(ans[i]) + 3
 
-	new_ans = pickNumber(new_ans_evaluated)
-	if ans[row] != new_ans:
-		if execPickedNumber(ans[row]) > new_ans_evaluated:
-			# reject new_ans
+	# jump forwawd or jump backward
+	if equation[row][row]:
+		new_ans_evaluated *= -1
+		new_ans = '"' + pickNumber(-new_ans_evaluated + 1) + '-'
+	else:
+		new_ans = pickNumber(new_ans_evaluated)
+
+	old_ans_evaluated = execPickedNumber(ans[row])
+	#print(old_ans_evaluated, new_ans_evaluated)
+	if old_ans_evaluated != new_ans_evaluated:
+		if abs(old_ans_evaluated) > abs(new_ans_evaluated):
+			if len(ans[row]) > len(new_ans):
+				ans[row] = new_ans
+				#print(row, "failue(C)", old_ans_evaluated, new_ans_evaluated)
+				return False
 			# fill with nop '0+'
-			while len(ans[row]) < len(new_ans):
-				ans[row] += '0+'
+			if len(ans[row]) < len(new_ans):
+				while len(ans[row]) < len(new_ans):
+					ans[row] += '0+'
+
+				if equation[row][row]:
+					#print(row, "failue(B)", old_ans_evaluated, new_ans_evaluated)
+					return False
 			# do next row
 			return solve_recur(equation, ans, row + 1)
 		else:
+
+			while len(ans[row]) > len(new_ans):
+				new_ans += '0+'
 			ans[row] = new_ans
 			# Redo from the beginning
 			# return failue
+			#print(row, "failue(A)", old_ans_evaluated, new_ans_evaluated)
 			return False
 	else:
 		# do next row
 		return solve_recur(equation, ans, row + 1)
 
-# @param equation [[False, True , 10],[True , True , 15],]
-# @return ['""++"+"+"+', '"""++"+"++""++']
+# @param equation [[False, True , 10],[True , True , 10],]
+# @return ['""+"+"+"+"++0+', '"""+"+"+"++""++-0+0+']
 def solve(equation):
 	if len(equation) > getrecursionlimit():
 		setrecursionlimit(len(equation) + 100)
@@ -63,40 +87,59 @@ def solve(equation):
 	return ans
 
 def getRandomEquation(dim, randmax):
-	ans = [[random() < 0.5 for _ in range(dim)] for _ in range(dim)]
+	eq = [[random() < 0.5 for _ in range(dim)] for _ in range(dim)]
 	for i in range(dim):
-		ans[i].append(randrange(randmax))
-	return ans
+		eq[i].append(randrange(randmax))
+	return eq
 
 if __name__ == '__main__':
 
+	equationList = []
+	_equationList = []
+
 	# example written at the beginning
-	equation = [
+	# ans is [33, -50]
+	_equationList.append([
 		[False, True , 10],
-		[True , True , 15],
-	]
+		[True , True , 10],
+	])
+	# ans is [43, -62]
+	_equationList.append([
+		[False, True , 20],
+		[True , True , 20],
+	])
+	_equationList.append([
+		[False, True , 100],
+		[True , True , 145],
+	])
 	# test fill nop
-	_equation = [
+	# ans is [0, -38]
+	_equationList.append([
 		[False, False,  0],
 		[False, True , 19],
-	]
+	])
 	# bigger equation
-	equation = [
+	_equationList.append([
 		[False, True , False, False, 100],
 		[True , True , False, True , 140],
 		[False, True , False, True ,  10],
 		[False, False, False, True , 100],
-	]
-	# much bigger equation in random state
-	# It takes a few seconds.
-	_equation = getRandomEquation(50, randrange(10000))
+	])
+	# equation in random state
+	# A Much bigger equation takes a few seconds.
+	equationList.append(getRandomEquation(100, randrange(100000)))
 
-	print('[')
-	for i in range(len(equation)):
-		print('\t', equation[i], ',', sep = '')
-	print(']')
-
-	ans = solve(equation)
-	print('ans :', ans)
-	print('eval:', [execPickedNumber(ans[i]) for i in range(len(ans))])
-	print('len :', [len(ans[i]) for i in range(len(ans))])
+	for equation in equationList:
+		print('[')
+		for i in range(len(equation)):
+			print('\t', equation[i], ',', sep = '')
+		print(']')
+		ans = solve(equation)
+		print('ans :', ans)
+		ans_evaluated = [execPickedNumber(ans[i]) for i in range(len(ans))]
+		print('eval:', ans_evaluated)
+		print('len :', [len(ans[i]) for i in range(len(ans))])
+		if equation == [[False, True , 10], [True , True , 10],] and ans_evaluated != [33, -50] or\
+		   equation == [[False, True , 20], [True , True , 20],] and ans_evaluated != [43, -62]:
+			print("INVALID")
+			exit(0)
